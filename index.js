e79b13d3b75eb41ab975b585ededd0ede072cef4
latifa ouali <latifaouali3003@gmail.com>
2023-12-14 13:43:17 +0100

    Fixed connection bug

--- index.js
+++ index.js
@@ -1,42 +1,50 @@
-const express=require('express');
-const config=require('config')
-const app=express();
-const cors=require('cors');
-const bodyParser=require('body-parser');
+const express = require("express");
+const config = require("config");
+const app = express();
+const cors = require("cors");
+const bodyParser = require("body-parser");
 app.use(bodyParser.json());
 app.use(cors());
 
-if(!config.get('VARjwtPrivateKey')) {
-    console.error('FATAL ERROR: VARjwtPrivateKey is not defined');
-    process.exit(1);
+if (!config.get("VARjwtPrivateKey")) {
+  console.error("FATAL ERROR: VARjwtPrivateKey is not defined");
+  process.exit(1);
 }
 
-const mongoose=require('mongoose');
-    //connection
-mongoose.connect('mongodb+srv://latifaouali3003:T8B78f6ygJQg0JwD@mymongodb.82dcac4.mongodb.net/mymongodb?retryWrites=true&w=majority/goShop')
-        .then(()=>{console.log('contected ...')})
-        .catch(err=>{console.error('err.message')})
+const mongoose = require("mongoose");
+//connection
+mongoose
+  .connect(
+    "mongodb+srv://latifaouali3003:T8B78f6ygJQg0JwD@mymongodb.82dcac4.mongodb.net/mymongodb?retryWrites=true&w=majority",
+    { useNewUrlParser: true, useUnifiedTopology: true }
+  )
 
+  .then(() => {
+    console.log("contected ...");
+  })
+  .catch((err) => {
+    console.error("err.message");
+  });
 
-const products=require('./routes/products');
-const users=require('./routes/user');
-const allUsers=require('./routes/users')
-const auth=require('./routes/auth');
-const reviews=require('./routes/review');
-const whishlists=require('./routes/wishlist');
-const carts=require('./routes/cart');
-const orders=require('./routes/orders');
-const contacts=require('./routes/contact');
+const products = require("./routes/products");
+const users = require("./routes/user");
+const allUsers = require("./routes/users");
+const auth = require("./routes/auth");
+const reviews = require("./routes/review");
+const whishlists = require("./routes/wishlist");
+const carts = require("./routes/cart");
+const orders = require("./routes/orders");
+const contacts = require("./routes/contact");
 
-app.use('/api/products',products);
-app.use('/api/users',users);
-app.use('/api/allUsers',allUsers)
-app.use('/api/auth',auth)
-app.use('/api/reviews',reviews)
-app.use('/api/whishlist',whishlists)
-app.use('/api/carts',carts);
-app.use('/api/orders',orders);
-app.use('/api/contacts',contacts);
+app.use("/api/products", products);
+app.use("/api/users", users);
+app.use("/api/allUsers", allUsers);
+app.use("/api/auth", auth);
+app.use("/api/reviews", reviews);
+app.use("/api/whishlist", whishlists);
+app.use("/api/carts", carts);
+app.use("/api/orders", orders);
+app.use("/api/contacts", contacts);
 
-const port=process.env.PORT||8081;
-app.listen(port,()=>console.log(`Server started on port ${port}`));
+const port = process.env.PORT || 8081;
+app.listen(port, () => console.log(`Server started on port ${port}`));
